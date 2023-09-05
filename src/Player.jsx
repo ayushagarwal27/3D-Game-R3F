36ff5c16068474841334177c57cbad922f8c221f
ayushagarwal27 <ayushagarwal.dev@gmail.com>
2023-09-05 21:58:03 +0530

    add player and controls

---
+++ src/Player.jsx
@@ -0,0 +1,76 @@
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { RigidBody } from "@react-three/rapier";
+import React, { useEffect, useRef } from "react";
+
+const Player = (state, delta) => {
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const bodyRef = useRef(null);
+
+  const jump = () => {
+    const origin = bodyRef.current.translation();
+    origin.y -= 0.31;
+    const direction = { x: 0, y: -1, z: 0 };
+    bodyRef.current.applyImpulse({ x: 0, y: 0.5, z: 0 });
+  };
+
+  useEffect(() => {
+    subscribeKeys(
+      (state) => state.jump,
+      (value) => {
+        if (value) {
+          jump();
+        }
+      }
+    );
+  }, []);
+
+  useFrame((state, delta) => {
+    const { forward, backward, leftward, rightward } = getKeys();
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impluseStrength = 0.6 * delta;
+    const torqueStrength = 0.2 * delta;
+
+    if (forward) {
+      impulse.z -= impluseStrength;
+      torque.x -= torqueStrength;
+    }
+    if (rightward) {
+      impulse.x += impluseStrength;
+      torque.z -= torqueStrength;
+    }
+    if (backward) {
+      impulse.z += impluseStrength;
+      torque.x += torqueStrength;
+    }
+    if (leftward) {
+      impulse.x -= impluseStrength;
+      torque.z += torqueStrength;
+    }
+
+    bodyRef.current.applyImpulse(impulse);
+    bodyRef.current.applyTorqueImpulse(torque);
+  });
+
+  return (
+    <RigidBody
+      position={[0, 1, 0]}
+      colliders="ball"
+      restitution={0.2}
+      friction={1}
+      linearDamping={0.5}
+      angularDamping={0.5}
+      canSleep={false}
+      ref={bodyRef}
+    >
+      <mesh castShadow>
+        <icosahedronGeometry args={[0.3, 1]} />
+        <meshStandardMaterial flatShading color="mediumpurple" />
+      </mesh>
+    </RigidBody>
+  );
+};
+
+export default Player;
